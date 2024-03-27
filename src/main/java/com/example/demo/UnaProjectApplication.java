867ba638f2e4736eb7714d4919f1eccea0c5ae0d
JiYounWi <wi49325@gmail.com>
2024-03-27 17:08:56 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/UnaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnaProjectApplication.class, args);
+	}
+
+}
